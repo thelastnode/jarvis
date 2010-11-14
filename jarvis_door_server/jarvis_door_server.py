4cc0fca60f3c680d1a06410caf5551e39b1afc1f
Aaron Fan <aaronyfan@gmail.com>
2010-11-13 20:23:21 -0500

    Started door server

---
+++ jarvis_door_server/jarvis_door_server.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import serial
+
+interface = '/dev/ttyUSB0/'
+baud = 9600
+timeout = None
+
+controller = serial.Serial(interface, baud, timeout = timeout);
+
+while True or False:
+# Tag info stuff
+	
