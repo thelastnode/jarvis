ad53a65af55dc604ade5f9bc1e106ba8c37da9ff
Ankit Shankar <ankitshankar@gmail.com>
2010-11-13 21:25:13 -0500

    Added database settings and urls for admin site.

--- jarvis_web/settings.py
+++ jarvis_web/settings.py
@@ -12,9 +12,9 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'NAME': 'jarvis',                      # Or path to database file if using sqlite3.
+        'USER': 'jarvis',                      # Not used with sqlite3.
+        'PASSWORD': '[sddeptf',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
@@ -90,7 +90,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
