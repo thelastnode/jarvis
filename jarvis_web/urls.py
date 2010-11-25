cf40b57d230d11f136aaacd3f575f57709773df9
The Last Bot 3000 <thelastbot3000@hal9000.(none)>
2010-11-25 15:16:15 -0500

    Add robots.txt

--- jarvis_web/urls.py
+++ jarvis_web/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import *
 from django.http import HttpResponseRedirect
+from django.views.generic.simple import direct_to_template
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -9,6 +10,7 @@ urlpatterns = patterns('',
     url(r'^login/$', 'jarvis_web.door_control.views.login', name='login'),
     url(r'^logout/$', 'jarvis_web.door_control.views.logout', name='logout'),
     (r'^door/', include('jarvis_web.door_control.urls')),
+    (r'^robots.txt', direct_to_template, { 'template': 'robots.txt' }),
     (r'^$', lambda request: HttpResponseRedirect('/door')),
     # Example:
     # (r'^jarvis_web/', include('jarvis_web.foo.urls')),
